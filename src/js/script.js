156f3f74c8590f17da7796e87eb3ee5b1b7bb305
yana <yarmenova@gmail.com>
2023-05-07 20:21:09 +0100

    removed fahrenheit link

--- src/js/script.js
+++ src/js/script.js
@@ -136,7 +136,7 @@ function showPosition(position) {
 function showCurrentTemp(response) {
   let temperature = Math.round(response.data.temperature.current);
   let p = document.querySelector("p");
-  p.innerHTML = `${temperature}℃`;
+  p.innerHTML = `${temperature}`;
   document.querySelector(
     "h1"
   ).innerHTML = `<i class="fa-sharp fa-solid fa-location-dot"></i> ${response.data.city}`;
@@ -167,32 +167,7 @@ function showCurrentWeather(event) {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 let currentButton = document.querySelector("#current");
 currentButton.addEventListener("click", showCurrentWeather);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("London");
